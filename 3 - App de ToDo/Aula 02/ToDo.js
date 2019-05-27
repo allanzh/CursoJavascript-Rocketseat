f781e81a506705ce06cd26fd867edcd4ca365132
Allan Cleysson de Oliveira Ramos <a.de.oliveira.ramos@avanade.com>
2019-05-26 21:34:36 -0300

    CMT Aula 02

--- 3 - App de ToDo/Aula 02/ToDo.js
+++ 3 - App de ToDo/Aula 02/ToDo.js
@@ -1,7 +1,9 @@
+// Carregando a lista input e botão 
 var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
+// Array de Tarefas
 var toDos = [
     'Fazer Café',
     'Estudar JS',
