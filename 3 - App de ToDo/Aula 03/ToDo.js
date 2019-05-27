bf7d9ae5740d2b11602f12754fb470bf59eb99f6
Allan Cleysson de Oliveira Ramos <a.de.oliveira.ramos@avanade.com>
2019-05-26 21:38:13 -0300

    CMT Aula 03

--- 3 - App de ToDo/Aula 03/ToDo.js
+++ 3 - App de ToDo/Aula 03/ToDo.js
@@ -1,19 +1,25 @@
+// Carregando a lista input e botão 
 var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
-
+// Array de Tarefas
 var toDos = [
     'Fazer Café',
     'Estudar JS',
     'Acessar a Comunidade Rocketseat'
 ];
 
+// Função para renderizar na tela o array
 function renderToDos() {
+    // foreach
     for(toDo of toDos){
+        
+        // Criando dinamicamente o elemento
         var toDoElement = document.createElement('li');
         var toDoText = document.createTextNode(toDo);
 
+        // Adicionando parentesco
         toDoElement.appendChild(toDoText);
         listElement.appendChild(toDoElement);
     }
