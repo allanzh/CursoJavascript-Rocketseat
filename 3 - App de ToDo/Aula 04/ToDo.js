6abde7f5e9b12e79dfab17b290d79abcdd49b4ac
Allan Cleysson de Oliveira Ramos <a.de.oliveira.ramos@avanade.com>
2019-05-26 21:42:55 -0300

    CMT Aula 04

--- 3 - App de ToDo/Aula 04/ToDo.js
+++ 3 - App de ToDo/Aula 04/ToDo.js
@@ -1,21 +1,30 @@
+// Carregando a lista input e botão 
 var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
-
+// Array de Tarefas
 var toDos = [
     'Fazer Café',
     'Estudar JS',
     'Acessar a Comunidade Rocketseat'
 ];
 
+// Função para renderizar na tela o array
 function renderToDos() {
+
+    // Antes de Atualizar as tarefas é necessário remover o que já existe
+    // dentro da lista para não ter tarefa duplicada;
     listElement.innerHTML = '';
 
+    // foreach
     for(toDo of toDos){
+        
+        // Criando dinamicamente o elemento
         var toDoElement = document.createElement('li');
         var toDoText = document.createTextNode(toDo);
 
+        // Adicionando parentesco
         toDoElement.appendChild(toDoText);
         listElement.appendChild(toDoElement);
     }
@@ -23,12 +32,17 @@ function renderToDos() {
 
 renderToDos();
 
+// Função para add tarefas digitadas
 function addToDo() {
+    
+    // Pegando valor digitado
     var toDoText = inputElement.value;
 
+    // Atualizando array, Limpando elemento e chamando a função para atualizar a lista
     toDos.push(toDoText);
     inputElement.value = '';
     renderToDos();
 }
 
+// Chamando função de add no clique do botão add
 buttonElement.onclick = addToDo;
