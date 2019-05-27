4a7b3de8bf3928817c4bb294cadf4966e070d1da
Allan Cleysson de Oliveira Ramos <a.de.oliveira.ramos@avanade.com>
2019-05-26 21:09:25 -0300

    Aula 02

---
+++ 3 - App de ToDo/Aula 03/ToDo.js
@@ -0,0 +1,9 @@
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
+
+var toDos = [
+    'Fazer Café',
+    'Estudar JS',
+    'Acessar a Comunidade Rocketseat'
+];
