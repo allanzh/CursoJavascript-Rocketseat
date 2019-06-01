3982a41ae682a6e86f918044150fee92c3288c34
Allan Cleysson de Oliveira Ramos <a.de.oliveira.ramos@avanade.com>
2019-06-01 17:00:13 -0300

    Aula 03 Modulo 04

--- 4 - JS Assíncrono/Aula 03/main.js
+++ 4 - JS Assíncrono/Aula 03/main.js
@@ -1,11 +1,11 @@
 // Utilizando Axios
 axios.get('https://api.github.com/users/allanzh')
     .then(function(response){
-        console.log(response); // Resolve cai aqui
+        console.log(response);
         // Axios já entende a response como javascript object, então podemos fazer:
         console.log(response.data.avatar_url);
         console.log(response.data.name);
     }) 
     .catch(function(error){
-        console.warn(error); // Reject cai aqui
+        console.warn(error);
     });
